af64beef12caeb9052e017a0f96f965aa3b7e2be
Emmanuel Solomo <emmanuel@webfutur.com>
2018-02-21 20:27:28 +0100

    update

--- views/dns.php
+++ views/dns.php
@@ -2,7 +2,7 @@
 error_reporting( E_ALL ); 
 ini_set( 'display_errors', 1 ); 
 ?><!DOCTYPE html>
-<html ng-app="dnsApp">
+<html ng-app="dnsApp" ng-cloak>
   <head>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.5/lodash.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
@@ -11,19 +11,19 @@ ini_set( 'display_errors', 1 );
     <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.9/angular-animate.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/1.0.3/angular-ui-router.min.js"></script>
     <script src="http://wordpress.betterdevops.co.uk/wp-content/plugins/betterdevops/js/dns.js"></script>
-    <link rel="stylesheet" href="http://wordpress.betterdevops.co.uk/wp-content/plugins/betterdevops/css/todo.css">
+    <script>	
+    window.WP = {
+    	      	plugin_url: "<?php echo $plugin_url; ?>",
+		          user_login: "<?php echo $user_login; ?>",
+		        user_name: "<?php echo $user_name; ?>"	     	
+	  }
+    </script> 
+    <link rel="stylesheet" href="http://wordpress.betterdevops.co.uk/wp-content/plugins/betterdevops/css/todo.css"></link>
   </head>
   <body>
       <div class="container">
         <ui-view />
-      </div>
-    <script>	
-    window.WP = {
-    	      	plugin_url: "<?php echo $plugin_url; ?>",
-		user_login: "<?php echo $user_login; ?>",
-		user_name: "<?php echo $user_name; ?>"	     	
-	}
-    </script>    
+      </div>   
   </body>
 </html>
 
