8a45b587cab7496e20bc39ce6994926799f8b64c
Emmanuel Solomo <emmanuel@webfutur.com>
2018-02-21 19:53:08 +0100

    update

--- js/dns.js
+++ js/dns.js
@@ -6,40 +6,7 @@ var dnsApp = angular.module('dnsApp', [
 	'ngSanitize',
 	'ui.router',
 	'ngAnimate'
-]);
-
-// Third-party support
-dnsApp.constant( '_', window._ );
-dnsApp.constant( 'WP', window.WP );
-
-// Routing
-dnsApp.config( function( $urlRouterProvider, $locationProvider ) {
-	$locationProvider.html5Mode( true );
-	$urlRouterProvider.otherwise( '/not-found/' );
-} );
-
-// Boot
-dnsApp.run( function( $rootScope, _ ) {
-  
-    // Log routing errors
-    $rootScope.$on( "$stateChangeError", console.error.bind( console, '$stateChangeError' ) );
-  
-    // Global lodash
-    $rootScope._ = window._;
-  } );
-
-// Route
-dnsApp.config( function( $stateProvider, WP ) {
-	$stateProvider.state( {
-		name: 'dns',
-		url: '/',
-		templateUrl: WP.plugin_url + '/views/dns2.html',
-		controller: 'DNSCtrl'
-	} );
-} );
-
-
-dnsApp.controller('TodoListController', function() {
+]).controller('TodoListController', function() {
     var todoList = this;
     todoList.todos = [
       {text:'learn AngularJS', done:true},
