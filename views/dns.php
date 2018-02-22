b0ca28040817721cb88c3b5579d3c23cacb63228
Emmanuel Solomo <emmanuel@webfutur.com>
2018-02-22 20:53:14 +0100

    update

--- views/dns.php
+++ views/dns.php
@@ -17,7 +17,6 @@ ini_set( 'display_errors', 1 );
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="http://wordpress.betterdevops.co.uk/wp-content/plugins/betterdevops/css/todo.css"></link>
     <link rel="stylesheet" href="http://wordpress.betterdevops.co.uk/wp-content/plugins/betterdevops/css/records.css"></link>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   </head>
   <body>
       <div class="container">
